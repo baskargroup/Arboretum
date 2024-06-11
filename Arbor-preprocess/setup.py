538ecbdc6cd53e41e33fdf6aa92f2895670a6dd2
znjubery <znjubery@gmail.com>
2024-06-11 10:27:57 -0500

    update readme

--- Arbor-preprocess/setup.py
+++ Arbor-preprocess/setup.py
@@ -1,24 +1,25 @@
 from setuptools import setup, find_packages
 
+# Read the requirements from the requirement.txt file
+with open('requirement.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='arbor_process',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
-    description='A package for preprocessing data in the Arbor project.',
+    description='A package for preprocessing data in the Arboretum project.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='Bella Yang',
-    author_email='your_email@example.com',
-    url='https://github.com/yourusername/arbor_process',
+    author='Baskar Group',
+    url='https://github.com/baskargroup/Arboretum/tree/main/Arbor-preprocess',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    install_requires=[
-        # List your package dependencies here
-    ],
+    install_requires=requirements,
     python_requires='>=3.6',
 )
 
